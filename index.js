29772acf664cd9dbf64d4ed5db6ce2a3d95d6fc8
TouhedurRahman <touhedur.cse@gmail.com>
2024-03-03 21:43:00 +0600

    create payment intent api added

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ require('dotenv').config();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 
+// This is your test secret API key.
+const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY);
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -153,6 +156,23 @@ async function run() {
         app.get('/promocodes', async (req, res) => {
             const result = await promocodesCollection.find().toArray();
             res.send(result);
+        });
+
+        // create payment intent api
+        app.post("/create-payment-intent", verifyJWT, async (req, res) => {
+            const { price } = req.body;
+            const amount = parseInt(price * 100);
+
+            // Create a PaymentIntent with the order amount and currency
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: "usd",
+                payment_method_types: ['card']
+            });
+
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            });
         });
 
         // Send a ping to confirm a successful connection
